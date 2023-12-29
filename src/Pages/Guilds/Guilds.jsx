728f1160912f891ca967a4389b663b7c1ad43e0e
Abir <abirmahmud221@gmail.com>
2023-12-29 12:12:32 +0600

    d :

--- src/Pages/Guilds/Guilds.jsx
+++ src/Pages/Guilds/Guilds.jsx
@@ -1,11 +1,29 @@
-
-
 const Guilds = () => {
-    return (
-        <div>
-            This is Guilds
+  return (
+    <div>
+        <h2 className="text-2xl text-center border w-4/12 mx-auto py-2 my-10">
+        Top Guilds
+      </h2>
+      {/* cards */}
+      <div >
+        <div className="card w-96 bg-base-100 shadow-xl image-full">
+          <figure>
+            <img
+              src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Guilds;
